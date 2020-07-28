f878ed91763a34d27a6215608c79c30e572f96d3
Caleb Thomas-Butler <calebt1@cityneighbors.org>
2020-07-28 18:20:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var rectangle- {
+width:200,
+hight:100
+};
+
+var a - 10, b - 11, c - 12, d - ¨hello¨
+
+
+function calcPerim(){
+  var width = rectangle.width
+  var hight = rectangle.hight
+  var result = width + hight + width + hight
+  //console.log(width);
+  return result;
+}
+
+function calcPerim(wid hei){
+  
+  var result = wid + hei + wid + hei
+  //console.log(width);
+  return result;
+}
+console.log(width);
+
+var theperim = calcPerim(2,3);
+console.log(¨the perimiter of the rectangle is¨+) ;
+
+console.log(width);
+
+
+function calcArea(){
+  var width = rectangle.width
+  var hight = rectangle.hight
+  var result = width + hight + width + hight
+  //console.log(width);
+  return result;
+}
+
+function calcArea(wid hei){
+  
+  var result = wid + hei + wid + hei
+  //console.log(width);
+  return result;
+}
